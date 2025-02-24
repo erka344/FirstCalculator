a79a2d027b5f8c1af213e473fe7291e446120f41
erka344 <erdeneochirerka344@gmail.com>
2025-02-23 17:24:43 -0800

    add

---
+++ calculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Calculator :Acalculator, IOperation
+    {
+        public void Add(double n)
+        {
+            Result += n;
+        }
+        public void Minus(double n)
+        {
+            Result -= n;
+        }
+    }
+}
---
+++ calculator/IOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    interface IOperation
+    {
+        void Add(double num);
+        void Minus(double num);
+    }
+}
